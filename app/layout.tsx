808970b48901852ee1fde1056a71190090dbdc1b
KalebDesta <kaleb27d@gmail.com>
2025-03-17 12:28:30 -0500

    build new issue page

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
       >
         <Theme>
           <NavBar></NavBar>
-          <main>{children}</main>
+          <main className="p-5">{children}</main>
         </Theme>
       </body>
     </html>
