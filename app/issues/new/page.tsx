4cb3af2ecf5c1b80c3bbac05e373692f5ebeae2c
KalebDesta <kaleb27d@gmail.com>
2025-03-17 14:37:01 -0500

    working issue create form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,16 +1,58 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
-import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import React from "react";
+import { Controller, useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
+import dynamic from "next/dynamic";
+
+// Dynamically import SimpleMDE with SSR disabled
+const SimpleMDE = dynamic(() => import("react-simplemde-editor"), {
+  ssr: false,
+});
+
+interface issueForm {
+  title: string;
+  description: string;
+}
 
 const NewIssuePage = () => {
+  const { register, handleSubmit, control } = useForm<issueForm>();
+  const router = useRouter();
+
   return (
-    <div className="space-y-3 max-w-xl">
-      <TextField.Root placeholder="Title"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
-      <Button>Create New Issue</Button>
-    </div>
+    <form
+      className="space-y-3 max-w-xl"
+      onSubmit={handleSubmit(async (data) => {
+        console.log(data);
+        const response = await fetch("/api/issues", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(data), // Convert to JSON string
+        });
+        if (response.ok) {
+          router.push("/issues");
+        } else {
+          console.error("Failed to submit issue", await response.text());
+        }
+      })}
+    >
+      <TextField.Root
+        placeholder="Title"
+        {...register("title")}
+      ></TextField.Root>
+      <Controller
+        control={control}
+        name="description"
+        render={({ field }) => {
+          return <SimpleMDE placeholder="Description" {...field} />;
+        }}
+      ></Controller>
+
+      <Button type="submit">Create New Issue</Button>
+    </form>
   );
 };
 
