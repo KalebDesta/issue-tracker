308a917e145f377d1e3cbe332724863a6597ee79
KalebDesta <kaleb27d@gmail.com>
2025-03-18 17:46:46 -0500

    error handling

--- app/issues/[id]/DeleteIssueButton.tsx
+++ app/issues/[id]/DeleteIssueButton.tsx
@@ -1,43 +1,63 @@
 "use client";
 import { Button, AlertDialog, Flex } from "@radix-ui/themes";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 const DeleteIssueButton = ({ issueId }: { issueId: number }) => {
   const router = useRouter();
-  return (
-    <AlertDialog.Root>
-      <AlertDialog.Trigger>
-        <Button color="red">Delete Issue</Button>
-      </AlertDialog.Trigger>
 
-      <AlertDialog.Content>
-        <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
-        <AlertDialog.Description>
-          This action cannot be undone. This will permanently delete the issue.
-        </AlertDialog.Description>
-        <Flex gap="2" align={"end"} mt={"4"}>
-          <AlertDialog.Cancel>
-            <Button variant="soft">Cancel</Button>
-          </AlertDialog.Cancel>
-          <AlertDialog.Action>
-            <Button
-              variant="solid"
-              color="red"
-              onClick={async () => {
-                await fetch(`/api/issues/${issueId}`, {
-                  method: "DELETE",
-                }).then(() => {
-                  router.push("/issues");
-                  router.refresh();
-                });
-              }}
-            >
-              Yes, Delete Issue
-            </Button>
-          </AlertDialog.Action>
-        </Flex>
-      </AlertDialog.Content>
-    </AlertDialog.Root>
+  const deleteIssue = async () => {
+    try {
+      await fetch(`/api/issues/${issueId}`, {
+        method: "DELETE",
+      }).then(() => {
+        router.push("/issues");
+        router.refresh();
+      });
+    } catch (error) {
+      setError(true);
+    }
+  };
+  const [error, setError] = useState(false);
+
+  return (
+    <>
+      <AlertDialog.Root>
+        <AlertDialog.Trigger>
+          <Button color="red">Delete Issue</Button>
+        </AlertDialog.Trigger>
+
+        <AlertDialog.Content>
+          <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+          <AlertDialog.Description>
+            This action cannot be undone. This will permanently delete the
+            issue.
+          </AlertDialog.Description>
+          <Flex gap="2" align={"end"} mt={"4"}>
+            <AlertDialog.Cancel>
+              <Button variant="soft">Cancel</Button>
+            </AlertDialog.Cancel>
+            <AlertDialog.Action>
+              <Button variant="solid" color="red" onClick={deleteIssue}>
+                Yes, Delete Issue
+              </Button>
+            </AlertDialog.Action>
+          </Flex>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+      <AlertDialog.Root open={error}>
+        <AlertDialog.Content>
+          <AlertDialog.Title>Error Occurred!</AlertDialog.Title>
+          <AlertDialog.Description>
+            There was an issue that prevented the issue from being deleted.
+          </AlertDialog.Description>
+
+          <Button variant="soft" onClick={() => setError(false)}>
+            OK
+          </Button>
+        </AlertDialog.Content>
+      </AlertDialog.Root>
+    </>
   );
 };
 
