1da501f105f380920d375364b3e4864ac6016f4d
KalebDesta <kaleb27d@gmail.com>
2025-03-18 08:10:12 -0500

    style the card

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Button, Callout, TextField } from "@radix-ui/themes";
+import { Button, Callout, Text, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import React, { useEffect, useState } from "react";
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const { join } = require("path");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("@tailwindcss/typography")], // ✅ Add the typography plugin
+};
