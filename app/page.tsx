ed4aa47e87afc3a6228fefff56d69a09caa80976
KalebDesta <kaleb27d@gmail.com>
2025-03-23 16:00:23 -0500

    build the latest issues

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,5 @@
-import Pagination from "./components/Pagination";
+import LatestIssues from "./LatestIssues";
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: { page: string };
-}) {
-  const validParams = await searchParams;
-  return (
-    <Pagination currentPage={parseInt(validParams.page) || 1} pageCount={10} />
-  );
+export default async function Home() {
+  return <LatestIssues />;
 }
